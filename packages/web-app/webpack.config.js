7df10cb85742e39539f11274392206cc35508735
GuoBin <guobin201314@gmail.com>
2020-08-14 15:28:56 +0800

    feat(webpack): webpack app

---
+++ packages/web-app/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path")
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "src/main.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: "[name]-bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: './html',
+      }
+    ]),
+  ],
+}
